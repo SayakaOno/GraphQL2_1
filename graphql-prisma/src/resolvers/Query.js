f1883aa1f971a5654ef0dafa060255458d96e4cf
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-09-19 21:42:40 -0700

    copy files and folders under graphql-basics into graphql-prisma

---
+++ graphql-prisma/src/resolvers/Query.js
@@ -0,0 +1,41 @@
+const Query = {
+  users(parent, args, { db }, info) {
+    if (!args.query) {
+      return db.users;
+    }
+    return db.users.filter(user =>
+      user.name.toLowerCase().includes(args.query.toLowerCase())
+    );
+  },
+  posts(parent, args, { db }, info) {
+    if (!args.query) {
+      return db.posts;
+    }
+    return db.posts.filter(post => {
+      return (
+        post.title.toLowerCase().includes(args.query.toLocaleLowerCase()) ||
+        post.body.toLowerCase().includes(args.query.toLocaleLowerCase())
+      );
+    });
+  },
+  comments(parent, args, { db }, info) {
+    return db.comments;
+  },
+  me() {
+    return {
+      id: 'abc123',
+      name: 'Sayaka',
+      email: 'sayaka@example.com',
+      age: 27
+    };
+  },
+  post() {
+    return {
+      id: 'id1',
+      title: 'post1',
+      body: 'body1',
+      published: true
+    };
+  }
+};
+export { Query as default };
