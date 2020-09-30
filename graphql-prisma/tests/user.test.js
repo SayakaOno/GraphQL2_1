863438652d7f799b3a63b6b7784219fd2264a252
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-09-29 23:24:17 -0700

    add tests for creating a user and queries

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -1,28 +1,107 @@
-import { getFirstName, isValidPassword } from '../src/utils/user';
+import 'cross-fetch/polyfill';
+import ApolloBoost, { gql } from 'apollo-boost';
+import bcrypt from 'bcryptjs';
+import prisma from '../src/prisma';
 
-it('Should return first name when given full name', () => {
-  const firstName = getFirstName('Sayaka Ono');
-
-  expect(firstName).toBe('Sayaka');
+const client = new ApolloBoost({
+  uri: 'http://localhost:4000'
 });
 
-it('Should return first name when given first name', () => {
-  const firstName = getFirstName('Jen');
-
-  expect(firstName).toBe('Jen');
+beforeEach(async () => {
+  await prisma.mutation.deleteManyPosts();
+  await prisma.mutation.deleteManyUsers();
+  const user = await prisma.mutation.createUser({
+    data: {
+      name: 'Jen',
+      email: 'jen@example.com',
+      password: bcrypt.hashSync('Red12345')
+    }
+  });
+  await prisma.mutation.createPost({
+    data: {
+      title: 'first post',
+      body: 'This is my very first post!',
+      published: true,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  });
+  await prisma.mutation.createPost({
+    data: {
+      title: 'second post',
+      body: 'I am still working on it...',
+      published: false,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  });
 });
 
-it('Should reject password shorter than 8 characters', () => {
-  const isValid = isValidPassword('short');
-  expect(isValid).toBe(false);
+it('Should create a new user', async () => {
+  const createUser = gql`
+    mutation {
+      createUser(
+        data: {
+          name: "Sayaka"
+          email: "sayaka@example.com"
+          password: "12345678"
+        }
+      ) {
+        token
+        user {
+          id
+        }
+      }
+    }
+  `;
+
+  const response = await client.mutate({
+    mutation: createUser
+  });
+  const exists = await prisma.exists.User({
+    id: response.data.createUser.user.id
+  });
+
+  expect(exists).toBe(true);
 });
 
-it('Should reject password that contains word password', () => {
-  const isValid = isValidPassword('longpassword');
-  expect(isValid).toBe(false);
+it('Should expose public author profiles', async () => {
+  const getUsers = gql`
+    query {
+      users {
+        id
+        name
+        email
+      }
+    }
+  `;
+  const response = await client.query({ query: getUsers });
+
+  expect(response.data.users.length).toBe(1);
+  expect(response.data.users[0].email).toBe(null);
+  expect(response.data.users[0].name).toBe('Jen');
 });
 
-it('Should correctly validate a valid password', () => {
-  const isValid = isValidPassword('1233abcd');
-  expect(isValid).toBe(true);
+it('Should expose published posts', async () => {
+  const getPosts = gql`
+    query {
+      posts {
+        id
+        title
+        body
+        published
+      }
+    }
+  `;
+
+  const response = await client.query({ query: getPosts });
+
+  expect(response.data.posts.length).toBe(1);
+  expect(response.data.posts[0].published).toBe(true);
 });
